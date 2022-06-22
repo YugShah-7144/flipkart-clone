45a3bd68c7f77f89ddfb277f59fc74c0208fb709
Yug Shah <yugshah7144@gmail.com>
2022-06-22 08:32:44 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,65 @@
-import React from 'react';
-// import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-// import SearchIcon from '@mui/icons-material/Search';
-import { AppBar, Toolbar, Box, styled, Typography } from '@mui/material';
-
-const NavBar = styled(AppBar)`
-    height: 55px;
-    background: #2874f0;
-`;
-
-const Component = styled(Box)`
-    margin-left: 12%;
-    line-height: none;
-`;
-
-const SubHeading = styled(Typography)`
-    font-size: 10px;
-    font-style: italic;
-`;
-
-
+import React from 'react'
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
+import "./Header.css";
 
 const Header = () => {
+  return (
+    <div className="header">
 
-    const logourl = "https://img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png";
-    const suburl = "//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png";
-    return (
-        <NavBar>
-            <Toolbar>
-                <Component>
-                    <img src={logourl} alt="logo" style={{ width: 75 }} />
-                    <Box>
-                        <SubHeading>
-                            Explore Plus
-                            <Box component='span'><img src={suburl} alt="sublogo" /></Box>
-                        </SubHeading>
-                    </Box>
-                </Component>
-            </Toolbar>
-        </NavBar>
+      {/* Flipkart logo */}
+      <div className="header__first">
+        <img
+          src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+          alt="flipkart logo"
+        />
+        <div className="header__first1">
+          <span
+            style={{
+              fontSize: "11px",
+              color: "white",
+              fontStyle: "italic",
+            }}
+          >
+            Explore
+          </span>
+          <span
+            style={{
+              color: "#f9e107",
+              fontSize: "11px",
+              fontStyle: "italic",
+            }}
+          >
+            Plus
+          </span>
+          <span>
+            <img
+              width="10"
+              src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png"
+              alt=""
+            />
+          </span>
+        </div>
+      </div>
 
-        // {/* <div className='Header'>
-        //     <div className="Header__first">
-        //         <img src="https://img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt="logo" />
-        //         <div className="Header__first1">
-        //             <span>Explore</span>
-        //             <span>Plus</span>
-        //             <span><img src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png" alt="" /></span>
-        //         </div>
-        //     </div>
-        //     <div className="Header__second">
-        //         <input type="text" />
-        //         <SearchIcon />
-        //     </div>
-        //     <div className="Header__third">
-        //         <button>Login</button>
-        //     </div>
-        //     <div className="Header__fourth">
-        //         <span>More</span>
-        //         <ExpandMoreIcon />
-        //     </div>
-        //     <div className="Header__fifth">
-        //         <ShoppingCartIcon />
-        //         <p>Cart</p>
-        //     </div>
-        // </div> */}
-    )
-}
+      {/* Search Area */}
+      <div className="header__second">
+        <input placeholder="Search for products, brands and more..." type="text" />
+        <SearchIcon />
+      </div>
+
+      {/* Login Button */}
+      <div className="header__third">
+        <button>Login</button>
+      </div>
+
+      {/* Cart */}
+      <div className="header__fourth">
+        <ShoppingCartIcon />
+        <p>Cart</p>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
